9ea73861cf8aaecc021eff33a4a71de2b0118fae
VaibhavTalkhande <vaibhavtalkhande41@gmail.com>
2024-01-26 19:04:11 +0530

    prompt

--- texttotext.js
+++ texttotext.js
@@ -7,8 +7,8 @@ async function run() {
   // For text-only input, use the gemini-pro model
   const model = genAI.getGenerativeModel({ model: "gemini-pro"});
 
-  const prompt = "Write a story about a magic backpack."
-
+  const prompt = "Write a story about ai destroying world"
+  console.log(prompt);
   const result = await model.generateContent(prompt);
   const response = await result.response;
   const text = response.text();
